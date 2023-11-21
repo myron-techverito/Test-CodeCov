d4ef0ac762ef63af061e0a0dd59806ac57572b95
Myron Dsilva <myron.dsilva@techverito.com>
2023-11-21 10:45:33 +0530

    adds tests for multiplication and subtraction

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,13 @@
-const { sum } = require("./sum");
+const { sum, sub, mul} = require("./sum");
 
 test("adds 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test("subtracts 2 from 1 to  equal 1", () => {
+    expect(sub(2, 1)).toBe(1);
+});
+
+test("multiplies 2 with 1 to  equal 2", () => {
+    expect(mul(2, 1)).toBe(2);
 });
